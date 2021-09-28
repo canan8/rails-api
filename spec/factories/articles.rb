96e6987b3195aba4a47c4e5e2951fe21956cf41b
Canan Ercan <cananercan96@gmail.com>
2021-09-28 16:12:37 +0300

    Add article model validations

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :article do
-    title { "MyString" }
-    content { "MyText" }
-    slug { "MyString" }
+    title { "Sample article" }
+    content { "Some content" }
+    sequence(:slug) { |n| "sample-article#{n}" }
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#validations' do
+    let(:article) { create(:article) }
+    
+    it 'validates title presence' do
+      article.title = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:title]).to include("can't be blank")
+    end
+
+    it 'validates content presence' do
+      article.content = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:content]).to include("can't be blank")
+    end
+
+    it 'validates slug presence' do
+      article.slug = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:slug]).to include("can't be blank")
+    end
+
+    it 'validates slug uniqueness' do
+      article1 = create(:article, slug: 'slug-test')
+      expect(article1).to be_valid
+
+      article2 = build(:article, slug: 'slug-test')
+      expect(article2).not_to be_valid
+    end
+  end
 end
