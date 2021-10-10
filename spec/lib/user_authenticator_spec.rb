5cb2ee441ff94057264fe51171b32f0fd357485d
Canan Ercan <cananercan96@gmail.com>
2021-10-10 21:11:04 +0300

    Add test for authenticator

--- spec/lib/user_authenticator_spec.rb
+++ spec/lib/user_authenticator_spec.rb
@@ -49,6 +49,11 @@ RSpec.describe UserAuthenticator do
         expect{ subject }.not_to change(User, :count)
         expect(authenticator.user).to eq(user)
       end
+
+      it 'should create and set user access token' do
+        expect{ subject }.to change(AccessToken, :count).by(1)
+        expect(authenticator.access_token).to be_present
+      end
     end
   end
 end
